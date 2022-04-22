944a0fe1c4da76f4effad292e4cc2404b6889602
chenmichael2 <chenmichael2012@gmail.com>
2022-04-22 16:54:50 -0700

    adding a form tag

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,6 +4,9 @@ function Login() {
     return (
         <div>
             <h1>Login Page</h1>
+            <form>
+                
+            </form>
             <Link href="/"><a><h2>Go Home</h2></a></Link>
         </div>
     )
