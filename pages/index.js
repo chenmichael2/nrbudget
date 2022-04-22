52c782251b7e337ded566bdbd0a0e3748878dd1f
chenmichael2 <chenmichael2012@gmail.com>
2022-04-21 17:20:03 -0700

    renaming index'

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href="/">Login</Link>
+      <Link href="/login">Login</Link>
     </div>
     // <div className={styles.container}>
       // {/* <Head>
