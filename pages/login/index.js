98949cdf95dabd09bf0ebc4d19770594296c3c0a
chenmichael2 <chenmichael2012@gmail.com>
2022-04-22 16:59:21 -0700

    adding a label for password?

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,8 +5,9 @@ function Login() {
         <div>
             <h1>Login Page</h1>
             <form>
-                <label>Username:</label>
+                <label>Username: </label>
                 <input type="text"></input>
+                <label></label>
             </form>
             <Link href="/"><a><h2>Go Home</h2></a></Link>
         </div>
