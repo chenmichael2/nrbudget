0bd1c519d0e73041e1a8abaa3881be578d01eb25
chenmichael2 <chenmichael2012@gmail.com>
2022-04-21 17:49:36 -0700

    getting rid of link href

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href="/login">Login</Link>
+      <Link>Login</Link>
     </div>
     // <div className={styles.container}>
       // {/* <Head>
