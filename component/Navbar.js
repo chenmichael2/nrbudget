cc9670ff5f7bebe7b9db19333aed5383e0ebb1cd
chenmichael2 <chenmichael2012@gmail.com>
2022-04-22 22:06:04 -0700

    adding the logo

--- component/Navbar.js
+++ component/Navbar.js
@@ -3,6 +3,9 @@
 function Navbar() {
     return (
         <nav>
+            <div className="logo">
+                <div>hello</div>
+            </div>
             <ul>
                 <li>Login</li>
             </ul>
