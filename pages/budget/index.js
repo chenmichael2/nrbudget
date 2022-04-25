dbd01931b0cf23e79f021c070611803ed5a70cdf
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 20:25:58 -0700

    adding the page container

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -2,7 +2,7 @@ import styles from "../../styles/Budget.module.css";
 
 function Budget() {
     return (
-        <div>
+        <div className={styles.pageContainer}>
             <div className={styles.budgetContainer}>
                 <h1>Budget Page</h1>
                 <h1>Welcome User!</h1>
