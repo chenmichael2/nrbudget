93d00d2e17fc23bb2e182f344a89e9ecc62bde06
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 20:00:11 -0700

    fixing my comment

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -1,8 +1,9 @@
 function Budget() {
     return (
         <div>
-            //
             <h1>Budget Page</h1>
+            {/* Things I want in my budget page */}
+            
         </div>
     )
 }
