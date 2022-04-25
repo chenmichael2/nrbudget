03c5adda66ef1b21f46930f9158426b26658dbdb
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 20:36:41 -0700

    adding the main section div?

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -4,12 +4,14 @@ function Budget() {
     return (
         <div className={styles.pageContainer}>
             <h1>Budget Page</h1>
-            <div className={styles.budgetContainer}>
-                <h1>Welcome User!</h1>
-                {/* Things I want in my budget page */}
-            </div>
             <div>
-                <h1>Info Section</h1>
+                <div className={styles.budgetContainer}>
+                    <h1>Welcome User!</h1>
+                    {/* Things I want in my budget page */}
+                </div>
+                <div>
+                    <h1>Info Section</h1>
+                </div>
             </div>
         </div>
     )
