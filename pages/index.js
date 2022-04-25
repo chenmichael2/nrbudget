5a404a6f296a3775a51fe845fcd4156e6c8e1053
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 19:25:48 -0700

    deleting comments

--- pages/index.js
+++ pages/index.js
@@ -7,28 +7,5 @@ export default function Home() {
     <div>
       <h1>Home Page</h1>
     </div>
-    // <div className={styles.container}>
-      // {/* <Head>
-      //   <title>Budget</title>
-      //   <meta name="description" content="Budget" />
-      // </Head> */}
-
-    //   <main className={styles.main}>
-    //     <h1>Hello</h1>
-    //   </main>
-
-    //   <footer className={styles.footer}>
-    //     <a
-    //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Powered by{' '}
-    //       <span className={styles.logo}>
-    //         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    //       </span>
-    //     </a>
-    //   </footer>
-    // </div>
   )
 }
