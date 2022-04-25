8a756ab1b4ec0643a9f1e832f89a2c75a1063f0f
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 20:13:05 -0700

    adding the welcome user section

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -2,8 +2,8 @@ function Budget() {
     return (
         <div>
             <h1>Budget Page</h1>
+            <h1>Welcome User!</h1>
             {/* Things I want in my budget page */}
-            
         </div>
     )
 }
