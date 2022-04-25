1cecf734bb8b4258ea703718f23e3b6e679c01b0
chenmichael2 <chenmichael2012@gmail.com>
2022-04-24 20:30:45 -0700

    cleaning up

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -3,8 +3,8 @@ import styles from "../../styles/Budget.module.css";
 function Budget() {
     return (
         <div className={styles.pageContainer}>
+            <h1>Budget Page</h1>
             <div className={styles.budgetContainer}>
-                <h1>Budget Page</h1>
                 <h1>Welcome User!</h1>
                 {/* Things I want in my budget page */}
             </div>
