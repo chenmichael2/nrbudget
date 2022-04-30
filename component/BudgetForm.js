c961d30bc2c86cc3a3d159c7eb8dfbe7e7833b35
chenmichael2 <chenmichael2012@gmail.com>
2022-04-30 00:08:10 -0700

    adding the new dropdown menu

--- component/BudgetForm.js
+++ component/BudgetForm.js
@@ -1,6 +1,6 @@
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import { DatePicker, Input, Picklist, Option, Modal } from "react-rainbow-components"; //react rainbow
+import { DatePicker, Input, Picklist, Option, Modal, GlobalHeader } from "react-rainbow-components"; //react rainbow
 import styles from '../styles/BudgetForm.module.css';
 import { useState } from 'react';
 
@@ -55,7 +55,34 @@ function BudgetForm() {
                     labelAlignment="left"
                     error="Category Required"
                 /> */}
-                
+                <div className="rainbow-m-bottom_xx-large rainbow-p-bottom_xx-large">
+                    {/* <GlobalHeader
+                        src="images/user/user1.jpg"
+                        className="rainbow-p-bottom_xx-large rainbow-m-bottom_xx-large"
+                    > */}
+                        <div className="rainbow-flex rainbow-align_right">
+                            <Picklist
+                                label="Category"
+                                labelAlignment="left"
+                                required
+                                id="picklist-3"
+                                placeholder="Choose Building"
+                                // onChange={value => setState({ value })}
+                                enableSearch
+                            >
+                                <Option name="option 1" label="All Buildings" />
+                                <Option name="option 2" label="New Building" />
+                                <Option name="header" label="Your Buildings" variant="header" />
+                                <Option name="option 3" label="Experimental" />
+                                <Option name="option 4" label="Bennet Towers" />
+                                <Option name="option 5" label="Empire State" />
+                                <Option name="option 6" label="Central Park" />
+                                <Option name="option 7" label="Chrysler" />
+                                <Option name="option 8" label="Plaza" />
+                            </Picklist>
+                        </div>
+                    {/* </GlobalHeader> */}
+                </div>
                 <Input
                     label="Income"
                     placeholder="Income"
