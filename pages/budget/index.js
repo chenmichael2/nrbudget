c8be8fb2fac07ca713ce51b5fbd301549b5c11dd
chenmichael2 <chenmichael2012@gmail.com>
2022-04-29 18:39:17 -0700

    adding the imports and the form

--- pages/budget/index.js
+++ pages/budget/index.js
@@ -32,59 +32,7 @@ function Budget() {
                         <h1>Info Section</h1>
                     </div>
                     <div>
-                        <Paper className={styles.paperForm} elevation={3}>
-                            <div className={styles.paperTitle}>
-                                <h1>Submit a Budget</h1>
-                            </div>
-                            <form> {/* action="url it is sending" */}
-                                <DatePicker
-                                    formatStyle="medium"
-                                    id="datePicker-1"
-                                    value={date}
-                                    onChange={onChange}
-                                    required
-                                    error="Date required" // this is causing the box to look red
-                                    isCentered={true}
-                                    label="Date"
-                                    labelAlignment="left"
-                                    placeholder="Date"
-                                    className={styles.datePicker}
-                                />
-                                <Input
-                                    label="Description"
-                                    placeholder="Description"
-                                    required
-                                    isCentered={true}
-                                    labelAlignment="left"
-                                    error="Description Required"
-                                />
-                                <Input
-                                    label="Category"
-                                    placeholder="Category"
-                                    required
-                                    isCentered={true}
-                                    labelAlignment="left"
-                                    error="Category Required"
-                                />
-                                <Input
-                                    label="Income"
-                                    placeholder="Income"
-                                    isCentered={true}
-                                    labelAlignment="left"
-                                />
-                                <Input
-                                    label="Debit"
-                                    placeholder="Debit"
-                                    isCentered={true}
-                                    labelAlignment="left"
-                                />
-                                <div className={styles.budgetButton}>
-                                    <Button onClick={() => {
-                                        onSubmit();
-                                    }} variant="contained">Budget</Button>
-                                </div>
-                            </form>
-                        </Paper>
+                        {/* form */}
                     </div>
                 </div>
             </div>
