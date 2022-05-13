54885368066aad6bfc63d001194abf6e3108ae9d
chenmichael2 <chenmichael2012@gmail.com>
2022-05-12 20:47:43 -0700

    add div container for paper

--- component/TopData.js
+++ component/TopData.js
@@ -3,6 +3,7 @@ import Paper from '@mui/material/Paper';
 function TopData() {
     return (
         <Paper>
+            <div></div>
             <h1>Total Balance</h1>
             <h1>Total Debt</h1>
             <h1>Total Income</h1>
