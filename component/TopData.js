f1e6b3b0b4ef1b6ae257cb39df6e1997c1d8b4de
chenmichael2 <chenmichael2012@gmail.com>
2022-05-17 22:29:56 -0700

    add classname

--- component/TopData.js
+++ component/TopData.js
@@ -13,7 +13,7 @@ function TopData() {
                     <h1>Total Debt</h1>
                     <h1>$0.00</h1>
                 </div>
-                <div className={}>
+                <div className={styles.data}>
                     <h1>Total Income</h1>
                     <h1>$0.00</h1>
                 </div>
