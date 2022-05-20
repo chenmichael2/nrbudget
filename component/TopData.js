c3b2cb2fe341b2ab433a38d5ab3a54295fe5862f
chenmichael2 <chenmichael2012@gmail.com>
2022-05-19 22:47:29 -0700

    font weight to 700 for datatype

--- component/TopData.js
+++ component/TopData.js
@@ -7,7 +7,7 @@ function TopData() {
             <div className={styles.topDataContainer}>
                 <div className={styles.data}>
                     <p className={styles.dataTitle}>Total Balance</p>
-                    <p className={styles.dataType}>$0.00</p>
+                    <p className={styles.dataType}>$100,000,000.00</p>
                 </div>
                 <div className={styles.data}>
                     <p className={styles.dataTitle}>Total Debt</p>
